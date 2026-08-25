966a0d77989702eec5535ec5498e96a33e1c2200
hurricanecarry <hurricanecarry@outlook.com>
2026-08-25 17:22:49 +0800

    一些普通测试 v1.1

--- playwright.config.js
+++ playwright.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   testDir: './test',
   timeout: 30000,
   use: {
-    baseURL: 'http://47.94.107.75',
+    baseURL: ' http://localhost:5173',
     headless: false,           // 显示浏览器窗口，看得到操作过程
     screenshot: 'only-on-failure',
   },
